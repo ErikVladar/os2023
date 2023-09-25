3c0db5c01f7b10623fe24b4744f379cf20c99a6d
Erik Vladar <erikvladar47@gmail.com>
2023-09-25 16:05:43 +0200

    working sleep

--- user/sleep.c
+++ user/sleep.c
@@ -2,9 +2,9 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-int main(){
-  if(argc!){
-     fprintf(2, “no arg\n”);
+int main(int argc, char *argv[]){
+  if(argc<2){
+     fprintf(2, "Usage: ...\n");
      exit(-1);
   }
   sleep(atoi(argv[1]));

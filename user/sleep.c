0693ec322f4bc88832eb9177351abcfa6e509e29
Erik Vladar <erikvladar47@gmail.com>
2023-09-25 12:01:27 +0200

    sleep 3

--- user/sleep.c
+++ user/sleep.c
@@ -1,6 +1,12 @@
-if(argc < 2){
-  fprintf(2, “Usage: sleep for a new sec…\n”);
-  exit(-1);
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(){
+  if(argc < 2){
+     fprintf(2, “Usage: sleep for a new sec…\n”);
+     exit(-1);
+  }
+  sleep(atoi(argv[1]));
+  exit(1);
 }
-sleep(atoi(argv[1]));
-exit(1);
